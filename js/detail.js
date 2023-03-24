fdce778b679c0250f6557064029bec587007f25c
Tuyen Tran <trandinhtuyen2018@gmail.com>
2023-03-24 12:59:13 +0700

    .

--- js/detail.js
+++ js/detail.js
@@ -1,5 +1,23 @@
 function myFunction(imgs) {
-    var expandImg = document.getElementById("bigImg");
+    let expandImg = document.getElementById("bigImg");
     expandImg.src = imgs.src;
-    expandImg.parentElement.style.display = "block";
+    imgs.style.border = "2px solid orange"
 }
+function swapImg(x){
+    let expandSmall = document.getElementById("mainImg");
+    expandSmall.src = x.src
+    x.style.border = "2px solid orange"
+}
+function removeBorder(a){
+    a.style.border = "none"
+}
+function openModel(imgs1){
+    let model = document.getElementById("modelId");
+    model.classList.add("openModel");
+}
+window.addEventListener('mouseup',function(event){
+    model = document.getElementById("modelId");
+    if(event.target !== model && event.target.parentNode !== model){
+        model.classList.remove("openModel");
+    }
+});
