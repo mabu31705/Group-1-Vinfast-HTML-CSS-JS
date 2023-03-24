87ac51d51a654820c94d45dc75ae93c4db8f4071
Tuyen Tran <trandinhtuyen2018@gmail.com>
2023-03-24 18:30:54 +0700

    .

--- js/detail.js
+++ js/detail.js
@@ -8,6 +8,44 @@ function swapImg(x){
     expandSmall.src = x.src
     x.style.border = "2px solid orange"
 }
+function textImg(n){
+    let textImg0 = document.getElementById("clothe0");
+    let textImg1 = document.getElementById("clothe1");
+    let textImg2 = document.getElementById("clothe2");
+    let textImg3 = document.getElementById("clothe3");
+    let textImg4 = document.getElementById("clothe4");
+    let textImg5 = document.getElementById("clothe5");
+    let textImg6 = document.getElementById("clothe6");
+    let textImg7 = document.getElementById("clothe7");
+    switch (n){
+        case 0:
+            swapImg(textImg0);
+            break;
+        case 1:
+            swapImg(textImg1);
+            break;
+        case 2:
+            swapImg(textImg2)
+            break;
+        case 3:
+            swapImg(textImg3)
+            break;
+        case 4:
+            swapImg(textImg4)
+            break;
+        case 5:
+            swapImg(textImg5)
+            break;
+        case 6:
+            swapImg(textImg6)
+            break;
+        case 7:
+            swapImg(textImg7)
+            break;
+    }
+
+
+}
 function removeBorder(a){
     a.style.border = "none"
 }
