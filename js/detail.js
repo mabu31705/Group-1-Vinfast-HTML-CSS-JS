6364634250284325858eb76c5a39d82c5fc411c6
Tuyen Tran <trandinhtuyen2018@gmail.com>
2023-03-24 15:28:47 +0700

    .

--- js/detail.js
+++ js/detail.js
@@ -1,5 +1,21 @@
 function myFunction(imgs) {
-    var expandImg = document.getElementById("bigImg");
+    let expandImg = document.getElementById("bigImg");
     expandImg.src = imgs.src;
-    expandImg.parentElement.style.display = "block";
+    imgs.style.border = "2px solid orange"
+}
+function swapImg(x){
+    let expandSmall = document.getElementById("mainImg");
+    expandSmall.src = x.src
+    x.style.border = "2px solid orange"
+}
+function removeBorder(a){
+    a.style.border = "none"
+}
+function openModel(imgs1){
+    let model = document.getElementById("modelId");
+    model.classList.add("openModel");
+}
+function removeModel(e){
+    model = document.getElementById("modelId");
+    model.classList.remove("openModel")
 }
