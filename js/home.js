8ca2315efb51ad323dbd17a89c9d1551c1deacbc
mabu31705 <mabu31705@gmail.com>
2023-03-27 09:12:44 +0700

    basic switch error duplicate

--- js/home.js
+++ js/home.js
@@ -1,10 +1,22 @@
-let titleRegister12 = document.getElementById("titleRegister");
-let titleRegister = titleRegister12.getElementsByClassName("title-register");
-let titleLogin1 = document.getElementById("titleLogin");
-let titleLogin = titleLogin1.getElementsByClassName("titleLogin");
-titleLogin.addEventListener(
-    "click", function (){
-        let current = document.getElementsByClassName("activeButton");
-        current.className = current[0].className.replace(" activeButton", "");
-        titleRegister.className += " activeButton";
-    });
+if(document.getElementById("test3")!=null){
+    let test1 = document.getElementById("test3").getElementsByClassName("test1");
+    for (let i = 0; i < test1.length; i++) {
+        test1[i].addEventListener("click", function() {
+            let test2 = document.getElementsByClassName("test2");
+            test2[0].className = test2[0].className.replace(" test2", " ");
+            this.className += " test2";
+        });
+    }
+}
+
+function showHideRegLog(){
+    let tr= document.getElementById("testRegister");
+    let tl=document.getElementById("testLogin");
+if(tr.style.display==="none"){
+    tr.style.display="flex";
+    tl.style.display="none";
+} else {
+    tr.style.display="none";
+    tl.style.display="flex";
+}
+}
